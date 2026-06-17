bcb49b8bf5b31d5e98404cb9b30d1fd48bde2352
Jaime Martínez <info@jaimemartinez.nl>
2026-06-17 23:02:27 +0200

    make license gpl-3

--- wp-simple-anti-spam.php
+++ wp-simple-anti-spam.php
@@ -1,10 +1,13 @@
 <?php
 /**
- * Plugin Name: Simple Anti Spam
+ * Plugin Name: WP Simple Anti Spam
  * Description: No config, no big complicated 3rd party spam check. Just simple local, maybe not 100% (but enough) anti-spam solution :)
- * Plugin URI: https://jaimemartinez.nl/open-source/anti-spam
+ * Plugin URI: https://jaimemartinez.nl/open-source/simple-anti-spam
  * Author: Jaime Martinez
  * Author URI: https://jaimemartinez.nl
+ * License: GPL v3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * 
  * Props URI: Danny van Kooten > https://gist.github.com/dannyvankooten/c6b3e08e0bd4178eca6acd05df18a67a
  *
  * @package WPSimpleAntiSpam
